19d89f854deb3bb7145f55b95a553613f5ba7856
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-03 16:51:15 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 for (var i = 1; i < 101; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
+    if (i % 5 == 0 && i % 3 == 0 && i % 6 == 0) {
+        document.getElementById("lista").innerHTML += i + "fizzbuzzboolean" + "<br>";
+    } else if (i % 3 == 0 && i % 5 == 0)  {
         document.getElementById("lista").innerHTML += i + "fizzbuzz" + "<br>";
-    } else if (i % 5 == 0) {
-        document.getElementById("lista").innerHTML += i + "buzz" + "<br>";
-    } else if (i % 3 == 0) {
+    } else if (i % 3 == 0 && i % 6 == 0) {
+        document.getElementById("lista").innerHTML += i + "fizzboolean" + "<br>";
+    }  else if (i % 5 == 0 && i % 6 == 0) {
+        document.getElementById("lista").innerHTML += i + "buzzboolean" + "<br>";
+    }  else if (i % 3 == 0) {
         document.getElementById("lista").innerHTML += i + "fizz" + "<br>";
-    } else {
+    }  else if (i % 5 == 0) {
+        document.getElementById("lista").innerHTML += i + "buzz" + "<br>";
+    }  else {
         document.getElementById("lista").innerHTML += i + "<br>";
     }
 }
